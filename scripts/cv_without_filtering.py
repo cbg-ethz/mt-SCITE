5bb2128bd49c8d473b7c38597a707b3bb1c88e08
Sophie <13159214+seidels@users.noreply.github.com>
2024-11-20 17:26:32 +0100

    Ensure output files are created in output dir

--- scripts/cv_without_filtering.py
+++ scripts/cv_without_filtering.py
@@ -14,10 +14,17 @@ def score_tree(mat, tree_path, n2, bin_path="./mtscite", seed=1, suffix="temp", 
     """
     n2 is the number of mutations in the true tree. The remaining mutations will be set as non-mutated in the LL computation
     """
-    np.savetxt(f'mat{suffix}.txt', mat, delimiter=' ')
+
+    matrix_file = os.path.join(output_dir, f'mat{suffix}.txt')
+    np.savetxt(matrix_file, mat, delimiter=' ')
+
+    output_prefix = os.path.join(output_dir, f'output{suffix}') 
+    #np.savetxt(f'mat{suffix}.txt', mat, delimiter=' ')
+    
     n, m = mat.shape
+    print("Shape of the mutation matrix used for error rate testing")
     print(n, m)
-    cmd = f"{bin_path} -i mat{suffix}.txt -n {n} -n2 {n2} -m {m} -t {tree_path} -r 1 -l 0 -fd 0.0001 -ad 0.0001 -cc 0.0 -s -a -o output{suffix} -seed {seed}"
+    cmd = f"{bin_path} -i {matrix_file} -n {n} -n2 {n2} -m {m} -t {tree_path} -r 1 -l 0 -fd 0.0001 -ad 0.0001 -cc 0.0 -s -a -o {output_prefix} -seed {seed}"
     result = run(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
 
     out = result.stdout
@@ -35,21 +42,25 @@ def score_tree(mat, tree_path, n2, bin_path="./mtscite", seed=1, suffix="temp", 
         f"Command output:\n{result.stdout}\nCommand error:\n{result.stderr}"
     )
     
-    # Remove intermediate files
-    os.remove(f'mat{suffix}.txt')
-    for file_path in glob.glob('learned*_*_map*.*'):
-        os.remove(file_path)
+    # Remove intermediate files from this step
+    os.remove(matrix_file)
+    os.remove(f'{output_prefix}.samples')
 
     return float(score)
 
-def learn_mtscite(mat, bin_path="./mtscite", l=200000, seed=1, suffix="temp", **kwargs):
+def learn_mtscite(mat, output_dir, suffix="temp", bin_path="./mtscite", l=200000, seed=1, **kwargs):
     n, m = mat.shape
+    print("Shape of the mutation matrix used for error rate learning")
     print(n,m)
     # Write mat
-    np.savetxt(f'mat{suffix}.txt', mat, delimiter=' ')
-    cmd = f"{bin_path} -i mat{suffix}.txt -n {n} -m {m} -r 1 -l {l}  -fd 0.0001 -ad 0.0001 -cc 0.0 -s -o learned{suffix} -seed {seed}"
+    matrix_file = os.path.join(output_dir, f'mat{suffix}.txt')
+    np.savetxt(matrix_file, mat, delimiter=' ')
+
+    output_prefix = os.path.join(output_dir, f'learned{suffix}') 
+    
+    cmd = f"{bin_path} -i {matrix_file} -n {n} -m {m} -r 1 -l {l} -max_treelist_size 1 -fd 0.0001 -ad 0.0001 -cc 0.0 -s -o {output_prefix} -seed {seed}"
     result = run(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
-    os.remove(f'mat{suffix}.txt')
+    os.remove(matrix_file)
     out = result.stdout
     out = out.split('\n')
     score = -1
@@ -65,21 +76,22 @@ def learn_mtscite(mat, bin_path="./mtscite", l=200000, seed=1, suffix="temp", **
         f"Command output:\n{result.stdout}\nCommand error:\n{result.stderr}"
     )
 
-    return f"learned{suffix}_map0.gv", float(score)
+    # remove unnecessary files and only return first map tree
+    #print(f'{output_prefix}_map0.newick')
+    os.remove(f'{output_prefix}.samples')
+    os.remove(f'{output_prefix}_map0.newick')
+    return f"{output_prefix}_map0.gv", float(score)
 
 def kfold_mtscite(data_path, k=3, rate=0., seed=42, **kwargs):
-    print(data_path)
     X = np.loadtxt(data_path, delimiter=' ')
 
     if X.shape[0] < 10: # don't learn
         return None
-
-    suffix = f"_{rate}"
     
     val_scores = []
     if k == 1:
-        tree_path, val_ll = learn_mtscite(X, **kwargs)
-        val_ll = score_tree(X, tree_path, suffix=suffix, **kwargs)
+        tree_path, val_ll = learn_mtscite(X, output_dir=output_dir, **kwargs)
+        val_ll = score_tree(X, tree_path, suffix=f'_{rate}', **kwargs)
         val_ll = val_ll / X.size
         val_scores.append(val_ll)
         return val_scores
@@ -88,13 +100,13 @@ def kfold_mtscite(data_path, k=3, rate=0., seed=42, **kwargs):
     for i, (train_index, test_index) in enumerate(kf.split(X.T)):
         # Learn on filtered training data
         train_data = X.T[train_index].T
+        #tree_path, train_ll = learn_mtscite(train_data, suffix=f"_{rate}_{i}", **kwargs)
         tree_path, train_ll = learn_mtscite(train_data, suffix=f"_{rate}_{i}", **kwargs)
         # Evaluate on complete test data
         test_data = X.T[test_index].T
         val_ll = score_tree(test_data, tree_path, X.shape[0], suffix=f"_{rate}", **kwargs)
         # Store ll on complete test data divided by its size
         val_scores.append(val_ll/test_data.size)
-        os.remove(f'learned{suffix}_{i}.samples')
 
     return val_scores
 
@@ -105,7 +117,7 @@ def score_error_rates(data_path, **kwargs):
     for mat in mats:
         if 'csv' in mat:
             error_rate = mat.split("/")[-1].split(".csv")[0] # read error rate from input file
-            out = kfold_mtscite(mat, **kwargs) # run CV for this error rate
+            out = kfold_mtscite(mat, rate=error_rate, **kwargs) # run CV for this error rate
             if out is not None:
                 scores[error_rate] = out
     return scores
@@ -134,7 +146,7 @@ if __name__ == "__main__":
 
     df_list = []
     for rep in range(r):
-        val_scores = score_error_rates(data_directory, bin_path=mtscite_bin, l=l, k=k, seed=rep)
+        val_scores = score_error_rates(data_directory, bin_path=mtscite_bin, l=l, k=k, seed=rep, output_dir=output_dir)
         df = pd.DataFrame(val_scores)
         df_list.append(df)
     full_df = pd.concat(df_list)
